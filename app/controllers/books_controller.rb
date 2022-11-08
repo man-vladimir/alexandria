d136d295dea2e631aeeabbf57af6c840ed222de1
Vladimir <manaenko1@gmail.com>
2022-11-08 16:39:57 +0200

    Add BooksController, BookPresenter and FieldPicker

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,11 @@
-class  BooksController  <  ApplicationController 
+class BooksController < ApplicationController
 
-  def  index 
-    render json: { data: Book.all }
-  end 
+  def index
+    books = Book.all.map do |book|
+      FieldPicker.new(BookPresenter.new(book, params)).pick
+    end
+
+    render json: { data: books }.to_json
+  end
 
 end
