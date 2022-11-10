02c513c1e56f12ce8da0b45fe3cf49bd9ee0827c
Vladimir <manaenko1@gmail.com>
2022-11-10 12:46:48 +0200

    Add Paginator

--- Gemfile
+++ Gemfile
@@ -28,11 +28,14 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'carrierwave'
 gem 'carrierwave-base64'
 
+gem 'kaminari'
+
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'pry'
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem  'rspec-rails'
   gem  'factory_bot_rails' 
--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -1,12 +1,16 @@
 class BasePresenter
   # Define a class level instance variable
+  @relations = []
+  @sort_attributes = []
+  @filter_attributes = []
   @build_attributes = []
 
   # Open the door to class methods
   class << self
     # Define an accessor for the class level instance
     # variable we created above
-    attr_accessor :build_attributes
+    attr_accessor :relations, :sort_attributes,
+                  :filter_attributes, :build_attributes
 
     # Create the actual class method that will
     # be used in the subclasses
@@ -14,6 +18,18 @@ class BasePresenter
     # the arguments passed in an array
     def build_with(*args)
       @build_attributes = args.map(&:to_s)
+    end
+
+    def related_to(*args)
+      @relations = args.map(&:to_s)
+    end
+
+    def sort_by(*args)
+      @sort_attributes = args.map(&:to_s)
+    end
+
+    def filter_by(*args)
+      @filter_attributes = args.map(&:to_s)
     end
   end
 
