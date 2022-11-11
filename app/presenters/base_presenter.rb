ddd82cf65b436ec2424d11393f8b8652b086c6dd
Vladimir <manaenko1@gmail.com>
2022-11-11 11:11:23 +0200

    Implement Representation Builders

--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -46,4 +46,17 @@ class BasePresenter
     @data
   end
 
+  def build(actions)
+    actions.each { |action| send(action) }
+    self
+  end
+
+  def fields
+    FieldPicker.new(self).pick
+  end
+
+  def embeds
+    EmbedPicker.new(self).embed
+  end
+
 end
